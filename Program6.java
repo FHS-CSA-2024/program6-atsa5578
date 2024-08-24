2dc89d9782ea1b510364f45fc141fe2f38f8ab8f
atsa5578 <andrew.j.tsai@gmail.com>
2024-08-23 21:20:58 -0700

    Update Program6.java

--- Program6.java
+++ Program6.java
@@ -1,9 +1,36 @@
 //import stuff here!
-
+import java.util.*;
 //Your code here
-
+public class Program6 {
+    public static void main(String[] args){
+        final double PI = 3.14159;
+        double radius = 0;
+        //scanner
+        Scanner formula = new Scanner (System.in);
+        //userinput
+        System.out.println("Enter the radius: ");
+        radius = formula.nextInt();
+        //formulas
+        radius = radius;
+        double diameter = 2 * radius;
+        double area = PI * radius * radius;
+        double circumference = PI * diameter;
+        //print
+        System.out.printf("The radius of the circle is " + "%.3f\n", radius);
+        System.out.printf("The diameter of the circle is " + "%.3f\n", diameter);
+        System.out.printf("The area of the circle is " + "%.3f\n", area);
+        System.out.printf("The circumference of the circle is " + "%.3f\n", circumference);
+        
+    }
+}
 //Paste console output below:
 /*
+Enter the radius: 
+22
+The radius of the circle is 22.000
+The diameter of the circle is 44.000
+The area of the circle is 1520.530
+The circumference of the circle is 138.230
 
 
 */
